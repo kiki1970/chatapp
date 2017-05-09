e467763d2d9559b7a296dcf9bb93de636d381c58
kiki1970 <maeda@hi.is.kit.ac.jp>
2017-05-09 17:36:58 +0900

    ログイン機能追加(再)

--- ChatDeTornado-master/ChatDeTornado-master/app.py
+++ ChatDeTornado-master/ChatDeTornado-master/app.py
@@ -6,10 +6,64 @@ import tornado.ioloop
 import tornado.web
 import tornado.websocket
 from tornado.web import url
+import tornado.escape
+import tornado.options
+from tornado.options import define, options
+
+import logging
+
+define("port", default=5000, type=int)
+define("username", default="user")
+define("password", default="pass")
+
+class BaseHandler(tornado.web.RequestHandler):
+
+    cookie_username = "username"
+
+    def get_current_user(self):
+        username = self.get_secure_cookie(self.cookie_username)
+        logging.debug('BaseHandler - username: %s' % username)
+        if not username: return None
+        return tornado.escape.utf8(username)
+
+    def set_current_user(self, username):
+        self.set_secure_cookie(self.cookie_username, tornado.escape.utf8(username))
+
+    def clear_current_user(self):
+        self.clear_cookie(self.cookie_username)
 
 
-class IndexHandler(tornado.web.RequestHandler):
+class AuthLoginHandler(BaseHandler):
 
+    def get(self):
+        self.render("login.html")
+
+    def post(self):
+        logging.debug("xsrf_cookie:" + self.get_argument("_xsrf", None))
+
+        self.check_xsrf_cookie()
+
+        username = self.get_argument("username")
+        password = self.get_argument("password")
+
+        logging.debug('AuthLoginHandler:post %s %s' % (username, password))
+
+        if username == options.username and password == options.password:
+            self.set_current_user(username)
+            self.redirect("/")
+        else:
+            self.write_error(403)
+
+
+class AuthLogoutHandler(BaseHandler):
+    
+    def get(self):
+        self.clear_current_user()
+        self.redirect('/')
+
+
+class IndexHandler(BaseHandler):
+    @tornado.web.authenticated
     def get(self, *args, **kwargs):
         face_pics = ['cat.gif', 'fere.gif', 'lion.gif']
         img_name = random.choice(face_pics)
@@ -41,18 +95,30 @@ class Application(tornado.web.Application):
 
     def __init__(self):
         BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-        tornado.web.Application.__init__(self,
-                                         [
-                                         url(r'/', IndexHandler, name='index'),
-                                         url(r'/chat', ChatHandler, name='chat'),
-                                         ],
-                                         template_path=os.path.join(BASE_DIR, 'templates'),
-                                         static_path=os.path.join(BASE_DIR, 'static'),
-                                         )
+        handlers = [
+            url(r'/', IndexHandler, name='index'),
+            url(r'/chat', ChatHandler, name='chat'),
+            url(r'/auth/login', AuthLoginHandler),
+            url(r'/auth/logout', AuthLogoutHandler),
+        ]
+        settings = dict(
+            cookie_secret='gaofjawpoer940r34823842398429afadfi4iias',
+            template_path=os.path.join(BASE_DIR, 'templates'),
+            static_path=os.path.join(BASE_DIR, 'static'),
+            login_url="/auth/login",
+            xsrf_cookies=True,
+            autoescape="xhtml_escape",
+            debug=True,
+            )
+        tornado.web.Application.__init__(self, handlers, **settings)
 
 
 if __name__ == '__main__':
+    tornado.options.parse_config_file(os.path.join(os.path.dirname(__file__), 'server.conf'))
+    tornado.options.parse_command_line()
     app = Application()
+    app.listen(options.port)
+    logging.debug('run on port %d in %s mode' % (options.port, options.logging))
     http_server = tornado.httpserver.HTTPServer(app)
     http_server.listen(8000)
     tornado.ioloop.IOLoop.instance().start()
